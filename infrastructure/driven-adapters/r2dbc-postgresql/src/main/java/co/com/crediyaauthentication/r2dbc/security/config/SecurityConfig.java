eef2c80e338d2f477c9299c9b7e200b52bddf620
malemartinez <martinezmayra605@gmail.com>
2025-09-13 12:12:38 -0500

    Se añade capa de filtros reactivos para validar autenticacion a endpoints

--- infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/crediyaauthentication/r2dbc/security/config/SecurityConfig.java
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/crediyaauthentication/r2dbc/security/config/SecurityConfig.java
@@ -1,27 +1,44 @@
 package co.com.crediyaauthentication.r2dbc.security.config;
 
+import co.com.crediyaauthentication.r2dbc.security.jwt.JwtAuthenticationManager;
+import co.com.crediyaauthentication.r2dbc.security.jwt.JwtServerAuthenticationConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
 
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
 
+    private final JwtAuthenticationManager jwtAuthenticationManager;
+    private final JwtServerAuthenticationConverter jwtServerAuthenticationConverter;
+
+    public SecurityConfig(JwtAuthenticationManager jwtAuthenticationManager, JwtServerAuthenticationConverter jwtServerAuthenticationConverter) {
+        this.jwtAuthenticationManager = jwtAuthenticationManager;
+        this.jwtServerAuthenticationConverter = jwtServerAuthenticationConverter;
+    }
+
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-        http
+
+        AuthenticationWebFilter jwtAuthFilter = new AuthenticationWebFilter(jwtAuthenticationManager);
+        jwtAuthFilter.setServerAuthenticationConverter(jwtServerAuthenticationConverter);
+
+        return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .authorizeExchange((exchanges) ->
-                        exchanges
-                                .anyExchange().permitAll()
+                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
+                .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/api/v1/auth/**").permitAll()
+                        .anyExchange().authenticated()
                 )
-                .httpBasic(withDefaults());
-        return http.build();
+                .addFilterAt(jwtAuthFilter, SecurityWebFiltersOrder.AUTHENTICATION)
+                .build();
 
     }
 
---
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/crediyaauthentication/r2dbc/security/jwt/JwtAuthenticationManager.java
@@ -0,0 +1,37 @@
+package co.com.crediyaauthentication.r2dbc.security.jwt;
+
+import co.com.crediyaauthentication.model.auth.gateways.TokenProviderPort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.ReactiveAuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationManager implements ReactiveAuthenticationManager {
+
+    private final TokenProviderPort jwtService;
+
+    @Override
+    public Mono<Authentication> authenticate(Authentication authentication) {
+        String authToken = authentication.getCredentials().toString();
+
+        String username = jwtService.extractSubject(authToken);
+        if (username == null || !jwtService.validateToken(authToken)) {
+            return Mono.empty();
+        }
+
+        List<String> roles = jwtService.extractRoles(authToken);
+
+        List<SimpleGrantedAuthority> authorities = roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .toList();
+
+        return Mono.just(new UsernamePasswordAuthenticationToken(username, null, authorities));
+    }
+}
